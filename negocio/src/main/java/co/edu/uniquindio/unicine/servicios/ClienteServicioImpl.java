89dcb1c1f769a80f07f95e7268d31cf016489265
Juan <juanes13916@gmail.com>
2022-11-23 12:17:51 -0500

    config heroku

--- negocio/src/main/java/co/edu/uniquindio/unicine/servicios/ClienteServicioImpl.java
+++ negocio/src/main/java/co/edu/uniquindio/unicine/servicios/ClienteServicioImpl.java
@@ -133,7 +133,7 @@ public class ClienteServicioImpl implements ClienteServicio {
         String paramet2 = tx.encrypt(""+zd.toInstant().toEpochMilli());
         //enviarCorreo(cliente);
 
-        
+
         emailServicio.enviarEmail("Registro en unicine", "Hola debe ir al siguiente enlace para activar la cuenta : unicine-prueba2.herokuapp.com/activar_cuenta.xhtml?p1="+paramet1+"&p2="+paramet2, cliente.getCorreo());
 
 
